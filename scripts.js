d1604262b538efb9f621c0ac5d18cb0f0e3767a4
LesleyMova <al02944351@tecmilenio.mx>
2023-11-27 21:44:50 -0600

    galerias

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function cambiarImagen(img) {
     var imgGrande = document.getElementById('imagen-grande');
     imgGrande.src = img.src;
-    imgGrande.style.width = "50%";
-    imgGrande.style.height = "50%";
+    imgGrande.style.width = "70%";
+    imgGrande.style.height = "70%";
 }
